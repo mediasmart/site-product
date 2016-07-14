d603b736630d35beed49c13a8a985237a4dd5df0
@soyjavi <javi.jimenez.villar@gmail.com>
2016-07-14 11:28:11 +0700

    Minimal changes

--- assets/javascripts/page.js
+++ assets/javascripts/page.js
@@ -5,13 +5,20 @@ window.onload = function() {
   var mediasmart = {
     el: {
       header: document.querySelectorAll('body > header')[0]
+    },
+
+    fn: {
+      scroll: function() {
+        var scroll = document.body.scrollTop;
+        var header = mediasmart.el.header;
+        header.setAttribute('class', scroll > header.offsetHeight ? 'scroll' : '');
+      }
     }
   };
 
+  // -- Decorators
+  mediasmart.fn.scroll();
+
   // -- Listeners
-  window.addEventListener('scroll', function (event) {
-    var scroll = document.body.scrollTop;
-    var header = mediasmart.el.header;
-    header.setAttribute('class', scroll > header.offsetHeight ? 'scroll' : '');
-  }, false)
+  window.addEventListener('scroll', mediasmart.fn.scroll, false);
 }
